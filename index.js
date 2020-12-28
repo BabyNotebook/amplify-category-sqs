1df391a36b9b78ac0a4ae3563f4d20e58c92e02d
Aleksander Hansson <hello@ahansson.com>
2020-12-27 23:39:08 -0800

    make plugin functional

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+const category = "sqs";
+const path = require("path");
+
 async function executeAmplifyCommand(context) {
-    let commandPath = path.normalize(path.join(__dirname, 'commands'));
-    if (context.input.command === 'help') {
-      commandPath = path.join(commandPath, category);
-    } else {
-      commandPath = path.join(commandPath, category, context.input.command);
-    }
-    
-    const commandModule = require(commandPath);
-    await commandModule.run(context);
+  let commandPath = path.normalize(path.join(__dirname, "commands"));
+  if (context.input.command === "help") {
+    commandPath = path.join(commandPath, category);
+  } else {
+    commandPath = path.join(commandPath, category, context.input.command);
   }
 
-  module.exports = {
-    executeAmplifyCommand,
-  };
+  const commandModule = require(commandPath);
+  await commandModule.run(context);
+}
+
+module.exports = {
+  executeAmplifyCommand
+};
